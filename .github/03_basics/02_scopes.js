b3ea262fec93e6cbc9306486122f28fd1c3ae11c
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2025-06-26 03:52:40 +0000

    scopes

--- .github/03_basics/02_scopes.js
+++ .github/03_basics/02_scopes.js
@@ -1,12 +1,46 @@
+// var c = 300 
+//{} cld as scope 
+
+/*
 if (true) {
     let a = 10
     const b = 20
     var c = 30
-
 }
+*/ 
+//through if condition whatever the thing is written it is cld as block scope 
+// in outside the if condition it cld as ( global scope ) 
 
 
 //console.log(a);
 //console.log(b);
 //console.log(c);
 
+/*
+let a = 300 
+
+if (true) {
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a);
+}
+
+for ( let i = 0; i< array.length; i++) {
+    const element = array[i]; 
+}
+*/
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+
+//************************Nested scope*********************** 
+
+
+function one(){
+    const username = "mayank"
+
+    function 
+    
+}
