72c5ba702978dd25994c4652644866fb73561f23
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2025-06-26 03:55:18 +0000

    scopes

--- .github/03_basics/02_scopes.js
+++ .github/03_basics/02_scopes.js
@@ -39,8 +39,40 @@ for ( let i = 0; i< array.length; i++) {
 
 
 function one(){
-    const username = "mayank"
+  const username = "mayank"
 
-    function 
-    
+function function two(){
+  const website = "youtube"
+  //console.log(username);
 }
+  //console.log(website);
+
+  two()
+  
+}
+//one()
+
+if (true) {
+  const username = "mayank2"
+  if(username === "mayank2"){
+    const website =" youtube"
+   // console.log(username = website);
+  }
+  //console.log(website); error comes because if conditon outer side it is written 
+}
+//console.log(username);
+
+
+// ++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++++
+console.log (addone(5))
+function addone(num){
+  return num + 1
+}
+//addone(5) // value comes 6 
+
+
+// addTwo(5) --- doesn't use before declaration first 
+const addTwo = function(num){
+  return num + 2
+}
+//addTwo(5)
