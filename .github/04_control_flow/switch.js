136829e0b55decc070be87bb954aadf26baeef2f
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2025-07-03 07:30:42 +0000

    switch

---
+++ .github/04_control_flow/switch.js
@@ -0,0 +1,54 @@
+// switch (key) {
+//     case value:
+
+//     break;
+
+//     default;
+//     break;
+
+// }
+
+const month = 3 
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        //break;
+    case 4:
+        console.log("April");
+       // break; use for break the control flow 
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+/*
+const month = April
+
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break; //use for break the control flow 
+
+    default:
+        console.log("default case match");
+        break;
+} */
+
+        
