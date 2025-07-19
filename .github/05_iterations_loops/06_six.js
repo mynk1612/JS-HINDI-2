e135ce2b1f6d334ede426e0d9a2914ba746a5f81
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2025-07-19 16:14:38 +0000

    06_six

--- .github/05_iterations_loops/06_six.js
+++ .github/05_iterations_loops/06_six.js
@@ -6,7 +6,39 @@
 
 // console.log(values);
 
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // myNums.filter( (num) => num > 4 )
+
+
+// const coding = ["js", "ruby", "java", "python", "cpp"
+
+//     const values = coding.forEach( (item) =>{
+//         // console.log(item);
+//         return item
+//     })
+
+//     console.log(values);
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => num > 4)
+// console.log(newNums);  // [5, 6, 7, 8, 9, 10]
+
+
+// const newNums = myNums.filter( (num) => {
+//      return num > 4
+// })
+
+
+// const newNums = []
+
+// myNums.forEach( () =? {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums);
 
